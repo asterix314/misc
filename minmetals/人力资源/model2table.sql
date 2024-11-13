dc756adca9368431d499d25a11ffa56d1abafdeb
Robert Zhang <asterix314@163.com>
2024-11-13 17:32:36 +0800

    model2table tool in duckdb

---
+++ minmetals/人力资源/model2table.sql
@@ -0,0 +1,93 @@
+-- ADS 表建模
+with numeric_t as (    -- 数值表
+    select table_name, table_cn, 1 + unnest(range(max(d))) as i  -- topic index
+    from (
+        select table_name, table_cn, len(unnest(topics).path) as d
+        from "D:\misc\minmetals\人力资源\人才管理.json"
+        where topics is not null) as foo
+    group by table_name, table_cn),
+non_numeric as (        -- 非数值表，如信息列表
+    select table_name, table_cn, unnest(columns) as c
+    from "D:\misc\minmetals\人力资源\人才管理.json"
+    where topics is null),
+models as (
+    select 
+        table_name,
+        table_cn,
+        'topic_' || i as column_name,
+        '话题' || i as column_cn,
+        'VARCHAR(255)' as data_type,
+        true as key
+    from numeric_t
+    union all
+    select
+        table_name,
+        table_cn,
+        c.column_name,
+        c.column_cn,
+        c.data_type,
+        c.key
+    from non_numeric),
+result as (
+    select *
+    from models
+    union all
+    select  -- 数值表增加数值列
+        table_name, 
+        table_cn, 
+        'val' as column_name,
+        '数值' as column_cn,
+        'DOUBLE' as data_type,
+        false as key
+    from (
+        select distinct table_name, table_cn
+        from numeric_t) as foo
+    union all
+    select      -- 全部表增加固定列
+        table_name, 
+        table_cn, 
+        unnest(['org_code', 'ord_name', 'biz_date']) as column_name,
+        unnest(['组织代码', '组织名称', '日期']) as column_cn,
+        unnest(['VARCHAR(255)', 'VARCHAR(255)', 'VARCHAR(255)']) as data_type,
+        false as key
+    from (
+        select distinct table_name, table_cn
+        from models) as foo)
+select 
+    row_number() over (order by table_name, key desc) as 序号,
+    '人力资源' as 业务域,
+    '每日' as 更新频率,
+    'ADS' as 域名,
+    table_name as 表英文名称,
+    table_cn as 表中文名称,
+    column_name as 字段英文名称,
+    column_cn as 字段中文名称,
+    column_cn as 字段说明,
+    data_type as 字段类型,
+    case key when true then '是' else '否' end as 是否主键
+from result
+
+
+
+-- 话题树
+with t as (
+    select table_name, table_cn, unnest(topics) as topic
+    from "D:\misc\minmetals\人力资源\人才管理.json"
+    where topics is not null),
+c as (
+    select category, unnest(items) as item
+    from "D:\misc\minmetals\人力资源\类别展开.json")
+select 
+    table_name as 表英文名称, 
+    table_cn as 表中文名称,
+    coalesce(c1.item, topic.path[1], '') as 话题1, 
+    coalesce(c2.item, topic.path[2], '') as 话题2, 
+    coalesce(c3.item, topic.path[3], '') as 话题3, 
+    coalesce(c4.item, topic.path[4], '') as 话题4,
+    topic.unit as 单位,
+    coalesce(topic.comment, '') as 说明
+from t left join c as c1 on topic.path[1] = '{' || c1.category || '}'
+    left join c as c2 on topic.path[2] = '{' || c2.category || '}'
+    left join c as c3 on topic.path[3] = '{' || c3.category || '}'
+    left join c as c4 on topic.path[4] = '{' || c4.category || '}'
+order by 1,2,3,4,5,6
